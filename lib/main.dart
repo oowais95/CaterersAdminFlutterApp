3300cac9e03c701705fedba14220631c169f2385
oowais95 <45528844+oowais95@users.noreply.github.com>
2024-12-14 13:54:16 +0500

    fixed bg image

--- lib/main.dart
+++ lib/main.dart
@@ -27,56 +27,35 @@ class _FoodCalculatorPageState extends State<FoodCalculatorPage> {
 
   final List<Map<String, Map<String, dynamic>>> menus = [
     {
-      "Beef Biryani / Pulao": {
-        "price": 2500,
-        "factor": 12.5
-      }, // change this to float 12.5
+      "Beef Biryani / Pulao": {"price": 2500, "factor": 12.5},
       "Chicken Karahi / Qorma": {"price": 1400, "factor": 8},
       "Rabri Kheer  / Lab e sheren": {"price": 1200, "factor": 8},
       "Naan Taftan": {"price": 400, "factor": 8},
       "Salad Raita": {"price": 300, "factor": 10},
     },
     {
-      "Chicken Tikka (Bihari/Malai/Balochi)": {
-        "price": 1300,
-        "factor": 4
-      }, // change this to float 12.5
-      "Beef Biryani / Pulao": {
-        "price": 2500,
-        "factor": 12
-      }, // change this to float 12.5
+      "Chicken Tikka (Bihari/Malai/Balochi)": {"price": 1300, "factor": 4},
+      "Beef Biryani / Pulao": {"price": 2500, "factor": 12},
       "Chicken Karahi / Qorma": {"price": 1400, "factor": 8},
       "Rabri Kheer  / Lab e sheeren": {"price": 1200, "factor": 8},
       "Naan Taftan": {"price": 400, "factor": 8},
       "Salad Raita": {"price": 300, "factor": 10},
     },
     {
-      "Chicken Tikka (Bihari/Malai/Balochi)": {
-        "price": 1300,
-        "factor": 4
-      }, // change this to float 12.5
-      "Beef Biryani / Pulao": {
-        "price": 2500,
-        "factor": 12
-      }, // change this to float 12.5
+      "Chicken Tikka (Bihari/Malai/Balochi)": {"price": 1300, "factor": 4},
+      "Beef Biryani / Pulao": {"price": 2500, "factor": 12},
       "Chicken Karahi / Qorma": {"price": 1400, "factor": 8},
       "Gajar ka Halwa": {"price": 1500, "factor": 8},
-      "Wonton / ": {"price": 25, "factor": 1}, // cater float here
+      "Wonton / ": {"price": 25, "factor": 1},
       "Naan Taftan": {"price": 400, "factor": 8},
       "Salad Raita": {"price": 300, "factor": 10},
     },
     {
       "Ch Reshmi Kabab": {"price": 1400, "factor": 6},
-      "Ch Dynamite Fry": {
-        "price": 1600,
-        "factor": 7
-      }, // change this to float 12.5
-      "Beef Yakhni Pulao/ Biryani": {
-        "price": 2400,
-        "factor": 12
-      }, //// change this to float 12.5
+      "Ch Dynamite Fry": {"price": 1600, "factor": 7},
+      "Beef Yakhni Pulao/ Biryani": {"price": 2400, "factor": 12},
       "Ch Boneless Handi": {"price": 1600, "factor": 10},
-      "Rabri Kheer / ..": {"price": 1200, "factor": 10}, // cater float here
+      "Rabri Kheer / ..": {"price": 1200, "factor": 10},
       "Gulab Jamun": {"price": 400, "factor": 10},
       "Salad Raita": {"price": 300, "factor": 15},
     }
@@ -87,9 +66,10 @@ class _FoodCalculatorPageState extends State<FoodCalculatorPage> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Lavish Food Quantity Calculator'),
-        backgroundColor: Colors.redAccent,
+        backgroundColor: const Color.fromARGB(255, 216, 166, 166),
       ),
       bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: const Color.fromARGB(255, 17, 17, 17),
         onTap: (index) {
           setState(() {
             myIndex = index;
@@ -106,72 +86,80 @@ class _FoodCalculatorPageState extends State<FoodCalculatorPage> {
               icon: Icon(Icons.food_bank_sharp), label: 'Menu 4'),
         ],
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            // Input for total number of guests
-            Row(
-              children: [
-                Expanded(
-                  child: TextField(
-                    keyboardType: TextInputType.number,
-                    decoration: InputDecoration(
-                      labelText: 'Total Number of Guests',
-                      border: OutlineInputBorder(),
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('lavishAppbg.jpg'),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              // Input for total number of guests
+              Row(
+                children: [
+                  Expanded(
+                    child: TextField(
+                      keyboardType: TextInputType.number,
+                      decoration: InputDecoration(
+                        labelText: 'Total Number of Guests',
+                        border: OutlineInputBorder(),
+                      ),
+                      onChanged: (value) {
+                        setState(() {
+                          totalNumberOfGuests = int.tryParse(value) ?? 0;
+                        });
+                      },
                     ),
-                    onChanged: (value) {
-                      setState(() {
-                        totalNumberOfGuests = int.tryParse(value) ?? 0;
-                      });
-                    },
                   ),
-                ),
-              ],
-            ),
-            SizedBox(height: 20),
-            // Table headers
-            Row(
-              children: [
-                Expanded(
-                    child: Text('Item',
-                        style: TextStyle(fontWeight: FontWeight.bold))),
-                Expanded(
-                    child: Text('Quantity',
-                        style: TextStyle(fontWeight: FontWeight.bold))),
-                Expanded(
-                    child: Text('Price',
-                        style: TextStyle(fontWeight: FontWeight.bold))),
-                Expanded(
-                    child: Text('Total',
-                        style: TextStyle(fontWeight: FontWeight.bold))),
-              ],
-            ),
-            Divider(),
-            // Food items list
-            Expanded(
-              child: ListView.builder(
-                itemCount: menus[myIndex].length,
-                itemBuilder: (context, index) {
-                  String itemName = menus[myIndex].keys.elementAt(index);
-                  int price = menus[myIndex][itemName]?['price'];
-                  double factor = menus[myIndex][itemName]?['factor'];
-
-                  int quantity = (totalNumberOfGuests / factor).ceil();
-                  int total = quantity * price;
-
-                  return Row(
-                    children: [
-                      Expanded(child: Text(itemName)),
-                      Expanded(child: Text(quantity.toString())),
-                      Expanded(child: Text(price.toString())),
-                      Expanded(child: Text(total.toString())),
-                    ],
-                  );
-                },
+                ],
               ),
-            ),
-          ],
+              SizedBox(height: 20),
+              // Table headers
+              Row(
+                children: [
+                  Expanded(
+                      child: Text('Item',
+                          style: TextStyle(fontWeight: FontWeight.bold))),
+                  Expanded(
+                      child: Text('Quantity',
+                          style: TextStyle(fontWeight: FontWeight.bold))),
+                  Expanded(
+                      child: Text('Price',
+                          style: TextStyle(fontWeight: FontWeight.bold))),
+                  Expanded(
+                      child: Text('Total',
+                          style: TextStyle(fontWeight: FontWeight.bold))),
+                ],
+              ),
+              Divider(),
+              // Food items list
+              Expanded(
+                child: ListView.builder(
+                  itemCount: menus[myIndex].length,
+                  itemBuilder: (context, index) {
+                    String itemName = menus[myIndex].keys.elementAt(index);
+                    int price = menus[myIndex][itemName]?['price'];
+                    double factor = menus[myIndex][itemName]?['factor'];
+
+                    int quantity = (totalNumberOfGuests / factor).ceil();
+                    int total = quantity * price;
+
+                    return Row(
+                      children: [
+                        Expanded(child: Text(itemName)),
+                        Expanded(child: Text(quantity.toString())),
+                        Expanded(child: Text(price.toString())),
+                        Expanded(child: Text(total.toString())),
+                      ],
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
